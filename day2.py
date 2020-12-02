d4c094355160af163f5caa44dd3f564869bc4d87
Mia Hatton <Mia.Hatton@ons.gov.uk>
2020-12-02 10:23:21 +0000

    part one complete

--- day2.py
+++ day2.py
@@ -1,7 +1,22 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec  2 08:36:08 2020
+# Day 2 - Password Philosphy
 
-@author: hattom
-"""
+# read input file
 
+with open("inputs/day2.txt") as f:
+    # create dictionary of policies and passwords
+    policy_passwords = [line.split(':') for line in f.readlines()]
+    
+valid_pws = 0
+
+# iterate through dictionary
+for [pol, pw] in policy_passwords:
+    pw = pw.replace('\n', '')
+    count, letter = pol.split(' ')
+    [min_count, max_count] = [int(x) for x in count.split('-')]
+    # count occurence of letter in password
+    counter = pw.count(letter)
+    if counter >= min_count and counter <= max_count:
+        valid_pws += 1
+        
+print(valid_pws)
+    
