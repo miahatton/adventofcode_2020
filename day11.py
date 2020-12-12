ea5b76cb8721beb2aa9bb8b2a62ed226f8afa524
M Hatton <53306899+miahatton@users.noreply.github.com>
2020-12-12 10:02:56 +0000

    day 12 part 1, still working on part 2

--- day11.py
+++ day11.py
@@ -1,7 +1,7 @@
 from funcs import read_input
 import numpy as np
 
-def list_surrounds(seat_array, i, j):
+def list_surrounds(seat_array: np.ndarray, i: int, j: int) -> np.ndarray:
     surround_pairs =  [(j+1, i-1), (j+1, i), (j+1, i+1), 
                        (j, i-1), (j, i+1), 
                        (j-1, i-1), (j-1, i), (j-1, i+1)]
@@ -14,19 +14,45 @@ def list_surrounds(seat_array, i, j):
             continue
     return surrounding_seats
 
-def occupy(seat_array):
+
+def surrounding_seats(seat_array, i, j, part_num):
+    # we want to try every combination of i, i+1, i-1, j, j+1, j-1 and track which operations we've done!
+    seats = []
+    ops = [(1, -1), (+1, 0), (1, 1), (0, -1), (0, 1), (-1,-1), (-1,0), (-1,1)]
+    for op in ops:
+        temp = [j + op[0],i + op[1]]
+        while temp[1] >= 0 and temp[0] >= 0:
+            try:
+                if seat_array[temp[0], temp[1]] != '.':
+                    seats.append(seat_array[temp[0], temp[1]])
+                    break
+                elif part_num == 2:
+                    temp[0] += op[0]
+                    temp[1] += op[1]
+                else:
+                    break
+            except IndexError:
+                break
+    return seats
+
+def occupy(seat_array: np.ndarray, part_num: int) -> np.ndarray:
+    if part_num == 1:
+        thresh = 4
+    else:
+        thresh = 5
     new_array = np.empty((seat_array.shape[0], seat_array.shape[1]), dtype = str)
     for j in range(seat_array.shape[0]): # loop through row numbers
         for i in range(seat_array.shape[1]): # loop through column numbers
             if seat_array[j, i] == '.':
                 new_array[j, i] = seat_array[j, i] 
-                continue # ignore the floor
-            # list surrounding seats
             else:
-                surround = list_surrounds(seat_array, i, j)
+                # list surrounding seats
+                surround = surrounding_seats(seat_array, i, j, part_num)
+                # count occupied around unoccupied
                 if seat_array[j,i] == 'L' and surround.count('#') == 0:
                         new_array[j, i] = '#'
-                elif seat_array[j, i] == '#' and surround.count('#') >= 4:
+                # count unoccupied around occupied
+                elif seat_array[j, i] == '#' and surround.count('#') >= thresh:
                         new_array[j, i] = 'L'
                 else:
                     new_array[j, i] = seat_array[j,i]
@@ -43,7 +69,8 @@ for seat_row in seats:
     
 seat_array = np.array(rows)
 
-while not np.array_equal(seat_array, occupy(seat_array)):
-    seat_array = occupy(seat_array)
+# select part number when calling occupy function
+while not np.array_equal(seat_array, occupy(seat_array, 2)):
+    seat_array = occupy(seat_array, 2)
     
 print(np.count_nonzero(seat_array == '#'))
---
+++ day12.py
@@ -0,0 +1,82 @@
+from funcs import read_input
+
+class Ship:
+    def __init__(self):
+        self.manhattan = [0,0] # east and north are positive
+        self.direction = 0  # E = 0, S = 90, W = 180, N = 270
+        self.position = {0: 0, 90: 0, 180: 0, 270: 0}
+        self.direction_map = dict(E = 0, S = 90, W = 180, N = 270)
+    def change_direction(self, rotate_dir: str, degrees: int):
+        while degrees > 0:
+            if rotate_dir == 'R':
+                self.direction += 90
+                if self.direction == 360:
+                    self.direction = 0
+            else:
+                self.direction -= 90
+                if self.direction < 0:
+                    self.direction = 270
+            degrees -= 90
+    def move(self, direction_in_instruction: str, distance: int):
+        if direction_in_instruction != 'F':
+            movement_dir = self.direction_map[direction_in_instruction]
+        else:
+            movement_dir = self.direction
+        self.position[movement_dir] += distance
+    def calculate_manhattan(self):
+        self.manhattan[0] = abs(self.position[0] - self.position[180])
+        self.manhattan[1] = abs(self.position[90] - self.position[270])
+    def read_instruction(self, step: int):
+        if step[0] in ['R', 'L']:
+            self.change_direction(step[0], int(step[1:]))
+        else:
+            self.move(step[0], int(step[1:]))
+            
+class ShipWithWaypoint(Ship):
+    def __init__(self):
+        super().__init__()
+        self.set_waypoint()
+    def set_waypoint(self):
+        self.waypoint_pos = {
+                0: self.position[0] + 10,
+                90: self.position[90],
+                180: self.position[180],
+                270: self.position[270] + 1
+            }
+    def change_direction(self, rotate_dir: str, degrees: int):
+        map_left_rotate = {0: 90, 90: 180, 180: 270, 270: 0}
+        map_right_rotate = {0: 270, 90: 0, 180: 90, 270: 180}
+        mapping = map_right_rotate if rotate_dir == 'R' else map_left_rotate
+        new_waypoint_dir = self.waypoint_pos.copy()
+        for key in new_waypoint_dir.keys():
+            new_waypoint_dir[key] = self.waypoint_pos[mapping[key]]
+        self.waypoint_pos = new_waypoint_dir
+    def move(self, direction_in_instructions: str, distance: int):
+        if direction_in_instructions == 'F':
+            for key in self.waypoint_pos.keys():
+                self.position[key] += self.waypoint_pos[key] * distance
+        else:
+            movement_dir = self.direction_map[direction_in_instructions]
+            self.waypoint_pos[movement_dir] += distance
+
+
+instructions = read_input('inputs/day12.txt')
+
+ship = ShipWithWaypoint()
+print("Ship position: ", end = '\t\t')
+print(ship.position)
+print("Waypoint position: ", end = '\t')
+print(ship.waypoint_pos)
+print()
+    
+for step in instructions:
+    print(step)
+    ship.read_instruction(step)
+    print("Ship position: ", end = '\t\t')
+    print(ship.position)
+    print("Waypoint position: ", end = '\t')
+    print(ship.waypoint_pos)
+    print()
+    
+ship.calculate_manhattan()
+print(sum(ship.manhattan))
