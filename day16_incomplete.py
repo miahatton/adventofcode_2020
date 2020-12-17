9885cc3ed00ceff31bad771de7f48011197e7de2
M Hatton <53306899+miahatton@users.noreply.github.com>
2020-12-17 07:50:39 +0000

    more work on day 16

--- day16_incomplete.py
+++ day16_incomplete.py
@@ -1,7 +1,7 @@
 # Day 16
 from typing import List, Dict
 
-with open('inputs/day16.txt') as f:
+with open('inputs/day16_test2.txt') as f:
     rules, ticket, nearby_tickets = f.read().split('\n\n')
     
 rules = {k: v.split(' or ') for k, v in [rule.split(': ') for rule in rules.split('\n')]}
@@ -12,6 +12,9 @@ for k, v in rules.items():
     # give rules format {rule: [[min, max], [min, max]]}
     rules[k][0] = [int(x) for x in rules[k][0].split('-')]
     rules[k][1] = [int(x) for x in rules[k][1].split('-')]
+
+for k, v in rules.items():
+    print(k, v)
 
 invalid_count = 0
 
@@ -52,15 +55,40 @@ for i in range(len(nearby_tickets)):
         if not check_validity(int(val), rules):
             valid = False
     if valid:
-        valid_tickets.append(ticket)
+        valid_tickets.append(nearby_tickets[i])
 
 # track which field could be which
 
 print(len(valid_tickets), " tickets left." )
+print(valid_tickets)
+possible_fields = {}
 
+for i in range(len(valid_tickets[0])):
+    position = [x[i] for x in valid_tickets]
+    print("Position", i, position)
+    position_fields = list(rules.keys())
+    for val in position:
+        for field in rules.keys():
+            if field in position_fields:
+                if not check_validity(int(val), {field: rules[field]}):
+                    position_fields.pop(position_fields.index(field))
+    possible_fields[i] = position_fields
+    print(f'Position {i} could be {possible_fields[i]}')
+   
+print(possible_fields)    
 
-for ticket in valid_tickets:
-    for val in ticket:
-        possible_fields = rules.keys()
-        
-    
+while all([len(x) == 1 for x in possible_fields.values()]):
+    unique = []
+    for k, v in sorted(possible_fields.items(), key = lambda x: len(x[1])):
+        if len(v) == 1:
+            unique.append(v[0])
+        else:
+            remove = []
+            for i in range(len(v)):
+                if v[i] in unique:
+                    remove.append(i)
+            for i in remove:
+                v.pop(i)
+            
+print(possible_fields)
+            
